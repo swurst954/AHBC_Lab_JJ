3d85c06f6bff4ac21d3b01df76cebd03e0eabe25
swurst954 <swurst6@gmail.com>
2018-12-17 17:27:14 -0500

    lab jj

---
+++ Scripts/Main.js
@@ -0,0 +1,17 @@
+ function clickEvent(){
+     console.log("Button was clicked!");
+ }
+
+function newDisplay() {
+alert("Hello there!");
+}
+
+var x = 5;
+var y = 12;
+var z = x + y;
+
+function lastButton(){
+
+    console.log( Boolean(z, 17));
+
+}
